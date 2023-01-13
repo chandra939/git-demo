600864241f218e76e387b2d81d79aecc1f8e2915
chandra939 <buddhachandrasekhar9@gmail.com>
2023-01-13 20:30:42 +0530

    second commit

--- src/test/java/gitHubprompt/githubcmdprompt/GitCmdPrompt.java
+++ src/test/java/gitHubprompt/githubcmdprompt/GitCmdPrompt.java
@@ -1,5 +1,13 @@
 package gitHubprompt.githubcmdprompt;
 
+import org.testng.annotations.Test;
+
 public class GitCmdPrompt {
+	
+	@Test
+	public void git() {
+		System.out.println("hi");
+		System.out.println("hello");
+	}
 
 }
